562c5686712a7df7e9f84e7dc9b572ad3f9b1eba
Anass Ferrak <45141652+TheLordA@users.noreply.github.com>
2021-02-11 22:55:43 +0100

    add navigation

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import OnBoardingScreen from "./screens/OnboardingScreen";
+import LoginScreen from "./screens/LoginScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator headerMode="none">
+				<Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
+				<Stack.Screen name="Login" component={LoginScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
