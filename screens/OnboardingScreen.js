e332bfc93873cd895f4ed6049691178c11455f7f
Anass Ferrak <45141652+TheLordA@users.noreply.github.com>
2021-02-11 23:41:17 +0100

    Complete OnBoarding Screen

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,16 +1,74 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
 
-const OnBoardingScreen = ({ navigation }) => {
+const Dots = ({ selected }) => {
+	let backgroundColor = selected ? "rgba(0, 0, 0, 0.8)" : "rgba(0, 0, 0, 0.3)";
+
 	return (
-		<View style={styles.container}>
-			<Text>OnBoarding Screen</Text>
-			<Button title="Click Here" onPress={() => navigation.navigate("Login")}></Button>
-		</View>
+		<View
+			style={{
+				width: 6,
+				height: 6,
+				marginHorizontal: 3,
+				backgroundColor,
+			}}
+		/>
 	);
 };
 
-export default OnBoardingScreen;
+const Skip = ({ ...props }) => (
+	<TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+		<Text style={{ fontSize: 16 }}>Skip</Text>
+	</TouchableOpacity>
+);
+
+const Next = ({ ...props }) => (
+	<TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+		<Text style={{ fontSize: 16 }}>Next</Text>
+	</TouchableOpacity>
+);
+
+const Done = ({ ...props }) => (
+	<TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+		<Text style={{ fontSize: 16 }}>Done</Text>
+	</TouchableOpacity>
+);
+
+const OnboardingScreen = ({ navigation }) => {
+	return (
+		<Onboarding
+			SkipButtonComponent={Skip}
+			NextButtonComponent={Next}
+			DoneButtonComponent={Done}
+			DotComponent={Dots}
+			onSkip={() => navigation.replace("Login")}
+			onDone={() => navigation.navigate("Login")}
+			pages={[
+				{
+					backgroundColor: "#a6e4d0",
+					image: <Image source={require("../assets/onboarding-img1.png")} />,
+					title: "Connect to the World",
+					subtitle: "A New Way To Connect With The World",
+				},
+				{
+					backgroundColor: "#fdeb93",
+					image: <Image source={require("../assets/onboarding-img2.png")} />,
+					title: "Share Your Favorites",
+					subtitle: "Share Your Thoughts With Similar Kind of People",
+				},
+				{
+					backgroundColor: "#e9bcbe",
+					image: <Image source={require("../assets/onboarding-img3.png")} />,
+					title: "Become The Star",
+					subtitle: "Let The Spot Light Capture You",
+				},
+			]}
+		/>
+	);
+};
+
+export default OnboardingScreen;
 
 const styles = StyleSheet.create({
 	container: {
