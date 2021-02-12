39fa3027a10289e7815263618e38f669dd86634b
Anass Ferrak <45141652+TheLordA@users.noreply.github.com>
2021-02-12 16:39:23 +0100

    implement first time app launch check

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import OnBoardingScreen from "./screens/OnboardingScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -8,14 +9,31 @@ import LoginScreen from "./screens/LoginScreen";
 const Stack = createStackNavigator();
 
 const App = () => {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator headerMode="none">
-				<Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
-				<Stack.Screen name="Login" component={LoginScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+	const [isFirstLaunched, setIsFirstLaunched] = useState(false);
+	useEffect(() => {
+		AsyncStorage.getItem("firstLaunch").then((value) => {
+			if (value === null) {
+				AsyncStorage.setItem("firstLaunch", "true");
+				setIsFirstLaunched(true);
+			} else {
+				setIsFirstLaunched(false);
+			}
+		});
+	}, []);
+
+	if (isFirstLaunched === null) return null;
+	else if (isFirstLaunched === true) {
+		return (
+			<NavigationContainer>
+				<Stack.Navigator headerMode="none">
+					<Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
+					<Stack.Screen name="Login" component={LoginScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		);
+	} else {
+		return <LoginScreen />;
+	}
 };
 
 export default App;
