5fff2872bf512a5ee7156a3488d1806f36ff9d31
Anass Ferrak <45141652+TheLordA@users.noreply.github.com>
2021-02-14 21:25:16 +0100

    Create SocialButton & Fix FormButton import path

--- components/FormButton.js
+++ components/FormButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, TouchableOpacity, StyleSheet } from "react-native";
-import { windowHeight, windowWidth } from "../utils/Dimensions";
+import { windowHeight, windowWidth } from "../utils/Dimension";
 
 const FormButton = ({ buttonTitle, ...rest }) => {
 	return (
